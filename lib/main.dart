2d31d7338773f0ab06158f763f6bfd07b3f65898
RenautMestdagh <renaut.mestdagh@hotmail.com>
2025-01-14 22:50:03 +0100

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
-import 'dart:io';  // Add this import to use exit()
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Color(0x00000000), // status bar color
-  ));
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DIGI Mobile',
-      home: Scaffold(), // Empty home page
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF062842)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
 }
